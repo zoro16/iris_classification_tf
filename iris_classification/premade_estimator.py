a2e60f0fc7297c13b36b6827e865d95813c05d35
Mohamed Abdelgadir <mohamed.saleh16@gmail.com>
2018-03-27 11:26:10 +0800

    train and test data

--- iris_classification/premade_estimator.py
+++ iris_classification/premade_estimator.py
@@ -1,16 +1,3 @@
-#  Copyright 2016 The TensorFlow Authors. All Rights Reserved.
-#
-#  Licensed under the Apache License, Version 2.0 (the "License");
-#  you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#   http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS,
-#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#  See the License for the specific language governing permissions and
-#  limitations under the License.
 """An Example of a DNNClassifier for the Iris dataset."""
 from __future__ import absolute_import
 from __future__ import division
@@ -40,14 +27,16 @@ def main(argv):
     # Build 2 hidden layer DNN with 10, 10 units respectively.
     classifier = tf.estimator.DNNClassifier(
         feature_columns=my_feature_columns,
-        hidden_units=[10, 10], # Two hidden layers of 10 nodes each.
+        hidden_units=[20, 20, 10], # Two hidden layers of 10 nodes each.
         n_classes=3 # The model must choose between 3 classes.
     )
 
     # Train the Model.
     classifier.train(
-        input_fn = lambda:iris_data.train_input_fn(train_x, train_y, args.batch_size),
-        steps=args.train_steps
+        input_fn = lambda:iris_data.train_input_fn(train_x,
+                                                   train_y,
+                                                   args.batch_size),
+        steps = args.train_steps
     )
 
     # Evaluate the model.
